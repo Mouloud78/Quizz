849604b9fd0b2de5aaef2de64cb4fffdd2b81684
OMouloud <mouloudouldali@yahoo.fr>
2025-09-27 21:28:35 -0400

    added radio and questions group

--- script.js
+++ script.js
@@ -16,6 +16,30 @@ function addQuizContent(questions) {
     questionText.textContent = currentQuestion.question;
     questionBlock.appendChild(questionText);
 
+    currentQuestion.options.forEach((option, index) => {
+      const inputGroup = document.createElement("div");
+      inputGroup.className = "quiz__input-group";
+
+      const radioInput = document.createElement("input");
+      radioInput.type = "radio";
+      radioInput.className = "quiz__radio-input";
+      radioInput.id = `
+      ${currentQuestion.id}-${option.value}
+      `;
+      radioInput.name = currentQuestion.id;
+      radioInput.value = option.value;
+      radioInput.checked = index === 0;
+
+      const label = document.createElement("label");
+      label.className = "quiz-label";
+      label.htmlFor = radioInput.id;
+      label.textContent = option.label;
+
+      inputGroup.appendChild(radioInput);
+      inputGroup.appendChild(label);
+      questionBlock.appendChild(inputGroup);
+    });
+
     fragment.appendChild(questionBlock);
   });
 
