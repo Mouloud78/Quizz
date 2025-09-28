6c0e749a4bb4570b374ee16fb5561346f24f87dc
OMouloud <mouloudouldali@yahoo.fr>
2025-09-27 21:16:00 -0400

    added fragment in js

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 import quizData from "./quizData.js";
-console.log(quizData)
+// console.log(quizData);
+
+const form = document.querySelector(".quiz-form");
+const fromSubmitBtn = document.querySelector(".quiz__submit-button");
+
+function addQuizContent(questions) {
+  const fragment = document.createDocumentFragment();
+
+  questions.forEach((currentQuestion) => {
+    const questionBlock = document.createElement("div");
+    questionBlock.className = "quiz__question-block";
+    questionBlock.id = currentQuestion.id;
+    const questionText = document.createElement("p");
+    questionText.className = "quiz__question";
+    questionText.textContent = currentQuestion.question;
+    questionBlock.appendChild(questionText);
+
+    fragment.appendChild(questionBlock);
+  });
+
+  form.insertBefore(fragment, fromSubmitBtn);
+}
+
+addQuizContent(quizData.questions);
